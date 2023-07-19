eede76fba071d23ab4962f8edf5a52f02a5aac4f
teja-ghub <139973674+teja-ghub@users.noreply.github.com>
2023-07-19 17:31:57 +0530

    Update streamlit_app.py
    added new header and lines of text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('omega 3 & Blueberry oatmeal')
+streamlit.text('kale, Spinach & Rocket Smoothie ')
+streamlit.text('Hard-Boiled Free-Range Egg')
