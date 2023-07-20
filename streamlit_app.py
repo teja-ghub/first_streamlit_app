4401547d0a6aa3c6914a0eec6e1fa65570f7f7c5
teja-ghub <139973674+teja-ghub@users.noreply.github.com>
2023-07-20 10:07:28 +0530

    Update streamlit_app.py
    added new text and images for menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 & Blueberry oatmeal')
-streamlit.text('kale, Spinach & Rocket Smoothie ')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 omega 3 & Blueberry oatmeal')
+streamlit.text('🥗 kale, Spinach & Rocket Smoothie ')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
