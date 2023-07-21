6d88ca56c55b7ff8cddda010d7d8336a8f25c5a2
teja-ghub <139973674+teja-ghub@users.noreply.github.com>
2023-07-21 12:27:20 +0530

    Update streamlit_app.py
    added header and result format to be json

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,9 @@ Fruits_selected = streamlit.multiselect("pick some fruits:",list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[Fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
+
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
