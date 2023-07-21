c6e058b96a4958a6b87eb6406ab19705b4a1aa66
teja-ghub <139973674+teja-ghub@users.noreply.github.com>
2023-07-21 22:30:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,7 @@ def get_fruityvice_data(this_fruit_choice):
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    return fruityvice_normalized
 
+streamlit.stop()
 
 #new function for api respnose
 streamlit.header("Fruityvice Fruit Advice!")
