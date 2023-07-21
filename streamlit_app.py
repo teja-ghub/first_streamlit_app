068e4e9204495917a029904ce05f01ae26e01c01
teja-ghub <139973674+teja-ghub@users.noreply.github.com>
2023-07-21 22:31:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ def get_fruityvice_data(this_fruit_choice):
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    return fruityvice_normalized
 
-streamlit.stop()
+
 
 #new function for api respnose
 streamlit.header("Fruityvice Fruit Advice!")
@@ -37,7 +37,7 @@ try:
       streamlit.dataframe(back_from_function)
 except URLerror as e:
     streamlit.error()
-
+streamlit.stop()
 streamlit.header("The fruit load list contains:")
 #snowflake related functions
 def get_fruit_load_list():
